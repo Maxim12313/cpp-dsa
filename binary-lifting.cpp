be1f4292d9b5bf316736e119c522ff517ba772bc
maxim12313 <maximkim16@gmail.com>
2024-11-14 11:46:10 -0500

    corrections

--- binary-lifting.cpp
+++ binary-lifting.cpp
@@ -19,17 +19,21 @@
 
 using namespace std;
 
-int l;
+int POW;
 int timer;
 vector<int> tin;
 vector<int> tout;
 vector<vector<int>> up;
 
+bool isAncestor(int u, int v) {
+    return tin[u] <= tin[v] && tout[u] >= tout[v];
+}
+
 void dfs(int curr, int prev, vector<vector<int>> &adjList) {
     tin[curr] = timer++;
 
     up[curr][0] = prev;
-    for (int pow = 1; pow <= l && up[curr][pow - 1] != -1; pow++)
+    for (int pow = 1; pow <= POW; pow++)
         up[curr][pow] = up[up[curr][pow - 1]][pow - 1];
 
     for (int next : adjList[curr])
@@ -38,39 +42,44 @@ void dfs(int curr, int prev, vector<vector<int>> &adjList) {
     tout[curr] = timer++;
 }
 
-void processAncestor(vector<vector<int>> &adjList, int root) {
-    int n = adjList.size();
-    timer = 0;
-    l = ceil(log2(n));
-    tin.resize(n);
-    tout.resize(n);
-    up.assign(n, vector<int>(l + 1, -1));
-    dfs(root, -1, adjList);
-}
-
-// true if u ancestor of v
-bool isAncestor(int u, int v) {
-    return tin[u] <= tin[v] && tout[u] >= tout[v];
-}
-
-int getKth(int u, int k) {
-    for (int i = 0; i <= l && u != -1; i++)
-        if (k & (1 << i))
-            u = up[u][i];
-    return u;
-}
-
-int getLca(int u, int v) {
+int getLCA(int u, int v) {
     if (isAncestor(u, v))
         return u;
     if (isAncestor(v, u))
         return v;
 
-    for (int i = 0; i <= l; i++)
-        if (!isAncestor(up[u][i], v))
-            u = up[u][i];
+    for (int pow = POW; pow >= 0; pow--)
+        if (!isAncestor(up[u][pow], v))
+            u = up[u][pow];
 
     return up[u][0];
+}
+
+// full and above if need LCA
+void getLCAUsage() {
+    int n, q;
+    cin >> n >> q;
+    POW = ceil(log2(n));
+    vector<vector<int>> adjList(n + 1);
+    for (int i = 2; i <= n; i++) {
+        int prev;
+        cin >> prev;
+        adjList[prev].push_back(i);
+    }
+    tin.resize(n + 1);
+    tout.resize(n + 1);
+    up.assign(n + 1, vector<int>(POW + 1, 0));
+    timer = 0;
+    tin[0] = timer++;
+    dfs(1, 0, adjList);
+    tout[0] = timer++;
+
+    while (q--) {
+        int u, v;
+        cin >> u >> v;
+        int res = getLCA(u, v);
+        cout << (res ? res : -1) << "\n";
+    }
 }
 
 // concise if just need get Kth
