2ca63341233f0680aa403e41999fa95e1f4461d2
maxim12313 <maximkim16@gmail.com>
2024-11-27 13:37:49 -0800

    had 1 indexing issues. Adopted so operations take 0 indexed but internally uses 1 indexing because easier math

--- range/fenwick-tree.cpp
+++ range/fenwick-tree.cpp
@@ -3,26 +3,33 @@
 
 using namespace std;
 
+// tree is 1 indexed, but operations work 0 indexed
 template <typename T> struct FenwickTree {
     vector<T> tree;
     FenwickTree(int n) {
-        tree.resize(n);
+        tree.resize(n + 1);
     }
-    T sumHelp(int k) {
+    FenwickTree(vector<int> &v) {
+        tree.resize(size(v) + 1);
+        for (int i = 0; i < size(v); i++)
+            add(i, v[i]);
+    }
+    T sum(int k) {
+        k++;
         T sum = T(0);
         while (k >= 1) {
             sum += tree[k];
-            // go to next closest power of 2
             k -= k & -k;
         }
         return sum;
     }
     // [l, r]
     T sum(int l, int r) {
-        return sumHelp(r) - sumHelp(l - 1);
+        return sum(r) - sum(l - 1);
     }
 
     void add(int k, T x) {
+        k++;
         while (k < tree.size()) {
             tree[k] += x;
             k += k & -k;
