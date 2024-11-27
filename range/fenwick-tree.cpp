07ec81ea2d23e6eb015e4a42f069998b3a095cc4
maxim12313 <maximkim16@gmail.com>
2024-11-27 13:26:49 -0800

    name edits

--- range/fenwick-tree.cpp
+++ range/fenwick-tree.cpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 
-template <typename T> class FenwickTree {
-public:
+template <typename T> struct FenwickTree {
     vector<T> tree;
     FenwickTree(int n) {
         tree.resize(n);
     }
-    T getSum(int k) {
+    T sumHelp(int k) {
         T sum = T(0);
         while (k >= 1) {
             sum += tree[k];
@@ -18,11 +17,12 @@ public:
         }
         return sum;
     }
-    T rangeSum(int l, int r) {
-        return getSum(r) - getSum(l - 1);
+    // [l, r]
+    T sum(int l, int r) {
+        return sumHelp(r) - sumHelp(l - 1);
     }
 
-    void update(int k, T x) {
+    void add(int k, T x) {
         while (k < tree.size()) {
             tree[k] += x;
             k += k & -k;
